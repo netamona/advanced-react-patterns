6e1d7c92d2bae54d50e73a5c91a8af9fc0d1e6b4
Netanella <netanella@monalabs.io>
2023-01-10 14:59:10 +0200

    ex 4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -11,16 +11,33 @@ function useToggle() {
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  return {
+    on,
+    toggle,
+    getTogglerProps: props => ({
+      'aria-pressed': on,
+      onClick: () => {
+        props.onClick?.()
+        toggle()
+      },
+      ...props,
+    }),
+  }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
